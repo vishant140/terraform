d48ea67813bc48a68c99bd130e9dcd3ca220353b
vis <155252015+internshipmentorbaba@users.noreply.github.com>
2024-01-07 16:39:51 +0530

    new commit 2

--- new.tf
+++ new.tf
@@ -1,1 +1,13 @@
-hello
+  GNU nano 7.2                         new.tf                         Modified
+provider "aws" {
+ 
+} 
+
+resource "aws_instance" "terraform_ec2" {
+  ami           = "ami-0062dbf6b829f04e1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-first-ec2"
+  }
+}
